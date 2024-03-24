5ee2d6114541a47cd2830a685b3e906d5a6da2d7
Sputchik <138970407+Sputchik@users.noreply.github.com>
2024-03-24 19:10:41 +0200

    0.9.11

--- pdi_urltobat.bat
+++ pdi_urltobat.bat
@@ -1,17 +1,16 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 :: Change url to yours if needed
-set "listURL=https://raw.githubusercontent.com/Sputchik/program-downloader/main/urls.txt"
 
-if exist "%TEMP%\" (
-    set "downloadPath=%TEMP%\"
-) else (
-    set "downloadPath=%~dp0"
-)
-curl -o "%downloadPath%urls.txt" -s %listURL%
+set "downloadPath=C:\Users\Sputchik\Downloads\"
+
 for /f "delims=" %%a in ('type "%downloadPath%urls.txt"') do (
-    for /f "tokens=1,2 delims==" %%b in ("%%a") do (
-        echo set "%%b=%%c"
-    )
+  set "line=%%a"
+  set "line=!line:"=\"!"
+  for /f "tokens=1* delims==" %%b in ("!line!") do (
+    echo set "%%b=%%c"
+  ) 
 )
+
 pause
