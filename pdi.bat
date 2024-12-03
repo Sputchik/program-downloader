2954b7e63bc746480805f68c0dbc91a3cc3107ef
Sputchik <sputchik@gmail.com>
2024-12-03 20:27:32 +0100

    cleanup bumb 3

--- pdi.bat
+++ pdi.bat
@@ -9,29 +9,24 @@ if %errorlevel% neq 0 (
 )
 
 set "origin=%~dp0"
-set "FetchedURLs=0"
+set FetchedURLs=0
 set "Activated=(Activated)"
+set "UserAgent=Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0"
 set "URLsURL=https://raw.githubusercontent.com/Sputchik/program-downloader/main/urls.txt"
 set "ChooseFolder=powershell -Command "(new-object -ComObject Shell.Application).BrowseForFolder(0,'Please choose a folder.',0,0).Self.Path""
+set "Extensions=msi;zip;iso"
 
-:start
+:Start
 
 if %FetchedURLs%==0 (
 	call :FetchURLs
 ) else (
-	goto :DescEchoInit
+	call :ClearSelected
+	goto :MAIN_MENU
 )
-
-:DescEchoInit
-set "selected_Revo^Uninstaller^Pro=0"
-set "selected_4KVideoDownloaderPlus=0"
-set "Extensions=msi;zip;iso"
-
-goto :MAIN_MENU
 
 :MAIN_MENU
 
-::cls
 echo Select category:
 echo [1] General Dependencies
 echo [2] Messengers
@@ -65,8 +60,10 @@ set "category=%~1"
 set "programs=%~2"
 
 :DISPLAY_LIST
+
 cls
 echo Category: !Category!
+
 set index=1
 
 for %%a in (%programs:;= %) do (
@@ -159,16 +156,18 @@ goto :eof
 :FetchURLs
 
 if exist "%TEMP%\" (
-	set "downloadPath=%TEMP%\"
+	set "downloadPath=%TEMP%\urls.txt"
 ) else (
-	set "downloadPath=%~dp0"
+	set "downloadPath=%~dp0urls.txt"
 )
-curl -o "%downloadPath%urls.txt" -s %URLsURL%
+curl -A "%UserAgent%" -s %URLsURL% -o "%downloadPath%"
 
 :: Define all variables from 'urls.txt' starting from the third line
-for /f "usebackq skip=2 tokens=1* delims==" %%a in ("%downloadPath%urls.txt") do (
+for /f "usebackq skip=2 tokens=1* delims==" %%a in ("%downloadPath%") do (
 	set "%%a=%%b"
 )
+
+set FetchedURLs=1
 
 goto :eof
 
@@ -176,7 +175,7 @@ goto :eof
 
 cls
 if "%Do%" == "0" (
-	goto :afterdownload
+	goto :AfterDownload
 )
 echo Checking internet connectivity...
 echo.
@@ -188,7 +187,7 @@ if !errorlevel! == 1 (
 	timeout /t 2 >nul
 ) else (
 	cls
-	goto :download
+	goto :Download
 )
 
 cls
@@ -222,13 +221,12 @@ if !errorlevel! == 1 (
 
 goto :eof
 
-:download
+:Download
 
-if exist "!origin!Programs" (
-	set "Do=1"
-) else (
-	mkdir "!origin!Programs"
+if not exist "%origin%Programs" (
+	mkdir "%origin%Programs"
 )
+
 
 for %%C in (%Categories%) do (
 	set "programs=!%%C!"
@@ -238,48 +236,43 @@ for %%C in (%Categories%) do (
 		set "prog=!program:^= !"
 
 		if "!selected_%%P!" == "1" (
-			:: Directly use the URL variable assumed to be correctly named and set
 			set "downloadUrl=!url_%%P!"
 
-			if not "!downloadUrl!" == "" (
-				:: Determine the file extension based on the program
-				set "fileExt="
+			if downloadUrl neq "" (
+				set FileExt=0
 				for %%E in (!Extensions!) do (
 					for %%I in (!%%E!) do (
-						if "!program!" == "%%I" set "fileExt=%%E"
+						if "!program!" == "%%I" set FileExt=%%E
 					)
 				)
 
 				:: Default to .exe if no specific extension is found
-				if "!fileExt!" == "" set "fileExt=exe"
+				if !FileExt! == 0 set FileExt=exe
+
 				echo Downloading !prog! from !downloadUrl!
-				echo.
-				:: Update the output file extension based on the determined file extension
-				curl -L "!downloadUrl!" -o "!CD!\Programs\!prog!.!fileExt!"
+				curl --progress-bar --retry 3 -A "%UserAgent%" -L "!downloadUrl!" -o "!CD!\Programs\!prog!.!FileExt!"
+				
 				echo.
 			) else (
-				echo Error: Download URL for !prog! is missing.
+				echo Error: Download URL for !prog! is missing..?
 			)
 		)
 	)
 )
 
-goto :afterdownload
-goto :eof
+:AfterDownload
+choice /C yn /N /M "Programs downloaded (%origin%Programs), Try installing them silently? (y/n) "
 
-:afterdownload
-choice /C yn /N /M "Programs downloaded (!origin!Programs), Try installing them silently? (y/n) "
-set err_install=%errorlevel%
+set DoneExe=0
+set DoneMSI=0
 
-if %err_install% == 1 (
+if !ErrorLevel! == 1 (
 	set DoneAll=0
-	set DoneExe=0
-	set DoneMSI=0
 	goto :DirCheck
 ) else (
-	cls
-	call :ClearSelected
-	goto :DescEchoInit
+	set DoneAll=1
+
+	goto :DirCheck
 )
 
 goto :eof
@@ -287,13 +280,13 @@ goto :eof
 :DirCheck
 
 ::EXE
-dir "!origin!Programs\*.exe" /b /a-d >nul 2>&1
+dir "%origin%Programs\*.exe" /b /a-d >nul 2>&1
 set err_exe=%errorlevel%
 ::MSI
-dir "!origin!Programs\*.msi" /b /a-d >nul 2>&1
+dir "%origin%Programs\*.msi" /b /a-d >nul 2>&1
 set err_msi=%errorlevel%
 ::ZIP
-dir "!origin!Programs\*.zip" /b /a-d >nul 2>&1
+dir "%origin%Programs\*.zip" /b /a-d >nul 2>&1
 set err_zip=%errorlevel%
 
 if %err_zip% == 0 (
@@ -311,9 +304,7 @@ if %err_zip% == 0 (
 	goto :MAIN_MENU
 )
 
-goto :pain
-
-:pain
+:Pain
 
 if %DoneAll% == 1 (
 	cd "%origin%"
@@ -330,19 +321,18 @@ if %DoneAll% == 1 (
 	choice /C 1234 /N /M " "
 
 	if errorlevel 4 (
-		rd /s /q "!origin!Programs"
+		rd /s /q "%origin%Programs"
 		exit
 
 	) else if errorlevel 3 (
-		call :ClearSelected
 		cls
-	   goto :start
+	   goto :Start
 
 	) else if errorlevel 2 (
 		
 		for /f "usebackq delims=" %%I in (`%ChooseFolder%`) do set "folder=%%I"
-		if "!folder!" NEQ "!origin!Programs" (
-			robocopy "!origin!Programs" "!folder!\Programs" /E /cOPY:DATSO /MOVE
+		if "!folder!" NEQ "%origin%Programs" (
+			robocopy "%origin%Programs" "!folder!\Programs" /E /cOPY:DATSO /MOVE
 		)
 		if %errorlevel% EQU 16 (
 			echo A serious error occurred. Possible "Access Denied."
@@ -358,11 +348,12 @@ if %DoneAll% == 1 (
 		cls
 
 	) else if errorlevel 1 (
-		rd /s /q "!origin!Programs"
+		rd /s /q "%origin%Programs"
 		cls
+		goto :Start
 	)
 	
-	goto :pain
+	goto :Pain
 
 ) else if %DoneMSI% == 1 (
 	if %err_exe% == 0 (
@@ -377,18 +368,18 @@ if %DoneAll% == 1 (
 		choice /C 123 /N /M " "
 
 		if !errorlevel! == 2 (
-			call :exe
+			call :EXE
 			if exist "C:\Users\%username%\Desktop" (
 				del "C:\Users\%username%\Desktop\*.lnk"
 			)
 		) else if !errorlevel! == 1 (
-			call :exe
+			call :EXE
 
 		)
 	)
 
 	set DoneAll=1
-	goto :pain
+	goto :Pain
 
 ) else if %DoneZip% == 1 (
 	if %err_msi% == 0 (
@@ -404,10 +395,10 @@ if %DoneAll% == 1 (
 		choice /C 123 /N /M " "
 
 		if !errorlevel! == 1 (
-			call :msi
+			call :MSI
 
 		) else if !errorlevel! == 2 (
-			call :msi
+			call :MSI
 			if exist "C:\Users\%username%\Desktop" (
 				del "C:\Users\%username%\Desktop\*.lnk"
 			)
@@ -415,7 +406,7 @@ if %DoneAll% == 1 (
 	)
 
 	set DoneMSI=1
-	goto :pain
+	goto :Pain
 
 ) else if %DoneZip% == 0 (
 
@@ -431,23 +422,23 @@ if %DoneAll% == 1 (
 		choice /C 123 /N /M " "
 
 		if errorlevel 2 (
-			call :zip
+			call :ZIP
 			if exist "C:\Users\%username%\Desktop" (
 				del "C:\Users\%username%\Desktop\*.lnk"
 			)
 		
 		) else if errorlevel 1 (
-			call :zip
+			call :ZIP
 		)
 	)
 
 	set DoneZip=1
-	goto :pain
+	goto :Pain
 )
 
 goto :eof
 
-:createShortcut
+:CreateShortcut
 
 set "exePath=%~1"
 set "shortcutName=%~2"
@@ -459,47 +450,51 @@ echo Set objShortcut = objShell.CreateShortcut(objShell.SpecialFolders("Programs
 echo objShortcut.TargetPath = "%exePath%" >> "%vbsFilePath%"
 echo objShortcut.Save >> "%vbsFilePath%"
 "%vbsFilePath%"
+
 del "%vbsFilePath%"
 goto :eof
 
-:zip
+:ZIP
+
+echo.
 
 set "zipf=TradingView;TranslucentTB;Gradle;Chromium;ThrottleStop"
+
 if exist "VCRedistributables 2005-2022.zip" (
-	echo Installing VCRedistributables
+	echo Installing VCRedistributables...
 	mkdir "VCRedistributables 2005-2022" 2>nul
 	tar -xf "VCRedistributables 2005-2022.zip" -C "VCRedistributables 2005-2022"
 	"%CD%\VCRedistributables 2005-2022\install_all.bat"
 	del "VCRedistributables 2005-2022.zip"
 )
 if exist "AltStore.zip" (
-	echo Installing AltStore
+	echo Installing AltStore...
 	mkdir AltStore 2>nul
 	tar -xf "AltStore.zip" -C "AltStore"
 	"%CD%\AltStore\setup.exe" /quiet
 )
 if exist "Autoruns.zip" (
-	echo Installing Autoruns
+	echo Installing Autoruns...
 	if exist "Autoruns" rd /s /q "Autoruns"
 	mkdir Autoruns 2>nul
-	tar -xf "!origin!Programs\Autoruns.zip" -C "Autoruns"
+	tar -xf "%origin%Programs\Autoruns.zip" -C "Autoruns"
 	mkdir "C:\Program Files\Autoruns" 2>nul
 	xcopy "%CD%\Autoruns\Autoruns64.exe" "C:\Program Files\Autoruns" /s /i /q /y
 	rd /s /q "Autoruns"
-	call :createShortcut "C:\Program Files\Autoruns\Autoruns64.exe" "Autoruns"
+	call :CreateShortcut "C:\Program Files\Autoruns\Autoruns64.exe" "Autoruns"
 )
 if exist "Telegram Portable.zip" (
-	echo Installing Telegram Portable
+	echo Installing Telegram Portable...
 	if exist "Telegram Portable" rd /s /q "Telegram Portable"
 	mkdir "Telegram Portable" 2>nul
 	tar -xf "Telegram Portable.zip" -C "Telegram Portable"
 	mkdir "C:\Program Files\Telegram" 2>nul
 	xcopy "%CD%\Telegram Portable\Telegram\*" "C:\Program Files\Telegram" /s /i /q /y
 	rd /s /q "Telegram Portable"
-	call :createShortcut "C:\Program Files\Telegram\Telegram.exe" "Telegram"
+	call :CreateShortcut "C:\Program Files\Telegram\Telegram.exe" "Telegram"
 )
 if exist "DirectX Runtimes Offline.zip" (
-	echo Installing DX Runtimes
+	echo Installing DX Runtimes...
 	if exist "DXO" rd /s /q "DXO"
 	mkdir "DXO" 2>nul
 	tar -xf "DirectX Runtimes Offline.zip" -C "DXO"
@@ -509,7 +504,7 @@ if exist "DirectX Runtimes Offline.zip" (
 	del "DirectX Runtimes Offline.zip"
 )
 if exist "Cinema4D 2024.3.2.zip" (
-	echo Installing Cinema4D
+	echo Installing Cinema4D...
 	if exist "Cinema4D" rd /s /q "Cinema4D"
 	mkdir "Cinema4D" 2>nul
 	tar -xf "Cinema4D 2024.3.2.zip" -C "Cinema4D"
@@ -518,20 +513,22 @@ if exist "Cinema4D 2024.3.2.zip" (
 	start /wait "Cinema4D_2024_2024.3.2_Win.exe"
 
 )
+
 cd "%origin%Programs"
+
 for %%A in (%zipf:;= %) do (
-	:: Replace '^' with a space for each program name
 	set "progName=%%A"
 	set "progName=!progName:^= !"
-	:: Check if the .zip file exists for the program
+
 	if exist "!progName!.zip" (
-		echo Installing !progName!
+		echo Installing !progName!...
 		mkdir "!progName!" 2>nul
 		tar -xf "!progName!.zip" -C "!progName!"
+
 		if "!progName!" == "Gradle" (
-			echo Installing Gradle
+			echo Installing Gradle...
+
 			for /d %%F in ("%origin%Programs\Gradle\*") do (
-				:: Move the directory
 				mkdir C:\Gradle 2>nul
 				if exist "C:\Gradle\%%F" (
 					echo Gradle Target directory "!targetPath!" already exists. Skipping...
@@ -541,6 +538,7 @@ for %%A in (%zipf:;= %) do (
 
 			)
 		)
+
 		mkdir "C:\Program Files\!progName!\" 2>nul
 		xcopy /s /e /y /i /q "!progName!" "C:\Program Files\!progName!"
 		if "!progName!" NEQ "Gradle" (
@@ -550,36 +548,31 @@ for %%A in (%zipf:;= %) do (
 )
 goto :eof
 
-:msi
-cd "!origin!Programs"
+:MSI
+
+cd "%origin%Programs"
+echo.
+
 for %%B in (!msi!) do (
 	set "progName=%%B"
 	set "progName=!progName:^= !"
 	if exist "!progName!.msi" (
-		echo Installing !progName!
+		echo Installing !progName!...
 		"!progName!.msi" /quiet
 	)
 )
 set "DoneMSI=1"
 goto :eof
 
-:exe
+:EXE
 
-cd "!origin!Programs"
-net session >nul 2>&1
-set "errorlvl=%errorlevel%"
-if %errorlvl% == 0 (
-	cd "!origin!Programs"
-) else (
-	echo Requesting administrative privileges...
-	echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin2.vbs"
-	echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" exe", "", "runas", 1 >> "%temp%\getadmin2.vbs"
-	"%temp%\getadmin2.vbs"
-	del "%temp%\getadmin2.vbs"
-	exit
-)
+cd "%origin%Programs"
+
 ren "Sideloadly.exe" "SideloadlySetup.exe" 2>nul
 ren "Librewolf.exe" "LibrewolfSetup.exe" 2>nul
+
+echo.
+
 if exist "3uTools.exe" (
 	echo Custom Install 3uTools ^(dumb shit has no slent install^)
 	ren "3uTools.exe" "3uTools_Setup.exe"
@@ -587,32 +580,32 @@ if exist "3uTools.exe" (
 )
 
 if exist "avidemux.exe" (
-	echo Installing AviDemux
+	echo Installing AviDemux...
 	start /wait "AviDemux.exe"
 )
 
 set "cmm=ContextMenuManager"
 if exist "%cmm%.exe" (
-	echo Installing ContextMenuManager
+	echo Installing ContextMenuManager...
 	mkdir "C:\Program Files\%cmm%" 2>nul
 	xcopy "%cmm%.exe" "C:\Program Files\%cmm%" /s /i /q /y
-	call :createShortcut "C:\Program Files\%cmm%\%cmm%.exe" "%cmm%"
+	call :CreateShortcut "C:\Program Files\%cmm%\%cmm%.exe" "%cmm%"
 
 )
 
 set "nv=NVCleanstall"
 if exist "%nv%.exe" (
-	echo Installing NVCleanstall
+	echo Installing NVCleanstall...
 	mkdir "C:\Program Files\%nv%" 2>nul
 	xcopy "%nv%.exe" "C:\Program Files\%nv%" /s /i /q /y
-	call :createShortcut "C:\Program Files\%nv%\%nv%.exe" "%nv%"
+	call :CreateShortcut "C:\Program Files\%nv%\%nv%.exe" "%nv%"
 )
 
 if exist "Rufus.exe" (
-	echo Installing Rufus
+	echo Installing Rufus...
 	mkdir "C:\Program Files\Rufus" 2>nul
 	xcopy "Rufus.exe" "C:\Program Files\Rufus" /s /i /q /y
-	call :createShortcut "C:\Program Files\Rufus\Rufus.exe" "Rufus"
+	call :CreateShortcut "C:\Program Files\Rufus\Rufus.exe" "Rufus"
 )
 
 if exist "Google Earth Pro.exe" (
@@ -622,13 +615,13 @@ if exist "WinRaR.exe" (
 	ren "WinRaR.exe" "WinRaR_Setup.exe"
 	call :RunInstaller "WinRaR_Setup" "/V"
 )
-:: Iterate through categories and programs
+
 for %%G in (S quiet VerySilent Q SilentNoRestart Vivaldi VS2022 WaterFox) do (
 	for %%I in (!Programs_%%G!) do (
 		set "ProgramName=%%I"
 		set "ProgramName=!ProgramName:^= !"
 		if exist "%CD%\!ProgramName!.exe" (
-			echo Installing !ProgramName!
+			echo Installing !ProgramName!...
 			echo.
 			call :RunInstaller "!ProgramName!" "!Flags_%%G!"
 		)
@@ -643,7 +636,6 @@ set "InstallerName1=%~1"
 set "Flags=%~2"
 set "InstallerName=!InstallerName1:^= !"
 
-:: Simulate installer command (replace with actual command to run the installer)
 start /wait "" "!InstallerName!.exe" !Flags!
 
 goto :eof
